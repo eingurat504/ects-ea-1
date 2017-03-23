df384a787f161b63d851caccc3502bd410147157
engurat504@gmail.com <eingurat504@gmail.com>
2017-03-23 19:58:37 +0300

    TCP Server Update

--- app/Console/Commands/Serverstart.php
+++ app/Console/Commands/Serverstart.php
@@ -37,46 +37,52 @@ class Serverstart extends Command
      */
     public function handle()
     {
-        //Reduce errors
-        error_reporting(~E_WARNING);
+  
+// by John Schimmel 
+// modified from the code at http://www.zend.com/pecl/tutorials/sockets.php
+// 
+// run this from terminal on mac os x or another command line interface.
 
-//Create a UDP socket
-        if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0)))
-        {
-            $errorcode = socket_last_error();
-            $errormsg = socket_strerror($errorcode);
+// Set time limit to indefinite execution 
+set_time_limit (0); 
 
-            die("Couldn't create socket: [$errorcode] $errormsg \n");
-        }
+// Set the ip and port we will listen on 
+$address = '127.0.0.1'; 
+$port = 9999; 
 
-//echo "SERVER HAS STARTED AND ITS LISTENING \n";
+// Create a TCP Stream socket 
+$sock = socket_create(AF_INET, SOCK_STREAM, 0); 
+echo "PHP Socket Server started at " . $address . " " . $port . "\n";
 
-// Bind the source address
-        if( !socket_bind($sock, "0.0.0.0" , 9999) )
-        {
-            $errorcode = socket_last_error();
-            $errormsg = socket_strerror($errorcode);
+// Bind the socket to an address/port 
+socket_bind($sock, $address, $port) or die('Could not bind to address'); 
+// Start listening for connections 
+socket_listen($sock); 
 
-            die("Could not bind socket : [$errorcode] $errormsg \n");
-        }
+//loop and listen
 
-//echo "Socket bind OK \n";
-        echo "SERVER ONLINE ... \n";
-//Do some communication, this loop can handle multiple clients
-        while(1)
-        {
-            //Receive some data
-            $r = socket_recvfrom($sock, $buf, 512, 0, $remote_ip, $remote_port);
-            echo "$remote_ip : $remote_port \n " . $buf;
-            //Send back the data to the client
-            socket_sendto($sock, "OK " . $buf , 100 , 0 , $remote_ip , $remote_port);
+while (true) {
+    /* Accept incoming requests and handle them as child processes */ 
+    $client = socket_accept($sock); 
+    
+    // Read the input from the client – 1024 bytes 
+    $input = socket_read($client, 1024); 
+    
+    // Strip all white spaces from input 
+    $output = ereg_replace("[ \t\n\r]","",$input)."\0"; 
+    
+    // Display output back to client 
+    socket_write($client, "you wrote " . $input . "\n"); 
+    
+    // display input on server side
+    echo "received: " . $input . "\n";
+}
 
+// Close the client (child) socket 
+socket_close($client); 
 
-        }
-
-        socket_close($sock);
-
-
+// Close the master sockets 
+socket_close($sock); 
 
     }
 }
